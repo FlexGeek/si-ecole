96e64a4f1b796c2c3a6906cabe45b13360540e08
Sidibe8 <yorosidibe@arrowbae.com>
2026-04-04 20:39:04 +0000

    first commit 2

--- config/db.js
+++ config/db.js
@@ -2,20 +2,25 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let pool;
 
-console.log('DB config:', {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD ? '***' : 'undefined'
-});
+if (process.env.DATABASE_URL) {
+  // Utilisation de l'URL fournie par Render (ou autre service cloud)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false // obligatoire pour Render
+    }
+  });
+} else {
+  // Environnement local (développement)
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
 
 module.exports = pool;
